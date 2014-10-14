e7da87d2e7322c38cf6d070b938809c1b803c46e
jrootham <jrootham@gmail.com>
2014-10-14 17:52:34 -0400

    Upload to got to meetup

--- argumentSpec.js
+++ argumentSpec.js
@@ -10,81 +10,101 @@
 
     var isArray = function (someVar) {
         return Object.prototype.toString.call( someVar ) === '[object Array]'
-    }
+    };
+
+    var validateArray = function (name, spec, argument) {
+        var result = [];
+        if (!isArray(argument)) {
+            result = result.concat(name + " is not an array");
+        }
+        else {
+            if (spec.length == 1) {
+                if (!argument.every(function (element) {
+                    var temp = validate('', spec[0], element)
+                    return 0 === temp.length
+                })) {
+                    result = result.concat(name + ' spec does not match some element');
+                }
+
+            }
+            else {
+                if (spec.length > 1) {
+                    if (spec.length != argument.length) {
+                        result = result.concat(name + " does not match length");
+                    }
+                    else {
+                        for(var i = 0 ; i < spec.length ; i++) {
+                            var temp = validate('', spec[i], argument[i])
+                            if (0 < temp.length) {
+                                result = result.concat(name + ' spec does not match element');
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    };
+
+    var validateObject = function (name, spec, argument) {
+        var result = [];
+        if (typeof argument !== typeof {}) {
+            result = result.concat(name + " is not an object");
+        }
+        else {
+        }
+
+        return result;
+    };
 
     var validate = function(name, spec, argument) {
 
         var result = [];
-        switch (typeof spec) {
-            case typeof true:
-                if (typeof argument !== typeof true) {
-                    result = result.concat(name + " is not a boolean");
-                }
-                break;
 
-            case typeof 0:
-                if (typeof argument !== typeof 0) {
-                    result = result.concat(name + " is not a number");
-                }
-                break;
-
-            case typeof '':
-                if (typeof argument !== typeof '') {
-                    result = result.concat(name + " is not a string");
-                }
-                else {
-                    if (spec.length > 0) {
-                        var regex = new RegExp(spec);
-                        if (!regex.test(argument)) {
-                            result = result.concat(name + " does not match regex");
-                        }
-                    }
-                }
-                break;
-
-            case typeof {}:                    // Objects and arrays
-                if(isArray(spec)) {
-                    if (!isArray(argument)) {
-                        result = result.concat(name + " is not an array");
-                    }
-                    else {
-                        if (spec.length == 1) {
-                            if (!argument.every(function (element) {
-                                var temp = validate('', spec[0], element)
-                                return 0 === temp.length
-                            })) {
-                                result = result.concat(name + ' spec does not match some element');
-                            }
-
-                        }
-                        else {
-                            if (spec.length > 1) {
-                                if (spec.length != argument.length) {
-                                    result = result.concat(name + " does not match length");
-                                }
-                                else {
-                                    for(var i = 0 ; i < spec.length ; i++) {
-                                        var temp = validate('', spec[i], argument[i])
-                                        if (0 < temp.length) {
-                                            result = result.concat(name + ' spec does not match element');
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                else {
-                    if (typeof argument !== typeof {}) {
-                        result = result.concat(name + " is not an object");
-                    }
-                    else {
-                    }
-                }
-                break;
-
+        if (spec === null) {
+            result = result.concat(name + " spec should not be null");
         }
+        else {
+            switch (typeof spec) {
+                case typeof true:
+                    if (typeof argument !== typeof true) {
+                        result = result.concat(name + " is not a boolean");
+                    }
+                    break;
+
+                case typeof 0:
+                    if (typeof argument !== typeof 0) {
+                        result = result.concat(name + " is not a number");
+                    }
+                    break;
+
+                case typeof '':
+                    if (typeof argument !== typeof '') {
+                        result = result.concat(name + " is not a string");
+                    }
+                    else {
+                        if (spec.length > 0) {
+                            var regex = new RegExp(spec);
+                            if (!regex.test(argument)) {
+                                result = result.concat(name + " does not match regex");
+                            }
+                        }
+                    }
+                    break;
+
+                case typeof {}:                    // Objects and arrays
+                    if (isArray(spec)) {
+                        result = result.concat(validateArray(name, spec, argument));
+                    }
+                    else {
+                        result = result.concat(validateObject(name, spec, argument));
+                    }
+                    break;
+            }
+        }
+
         return result;
     };
 
--- test/scalar.js
+++ test/scalar.js
@@ -7,33 +7,33 @@
  */
 (function () {
     "use strict"
-    var assert = require("assert");
+    var expect = require("chai").expect;
     var argumentSpec = require('../argumentSpec.js');
 
     describe('Scalar argument spec', function(){
         describe('boolean tests', function(){
             it('should return [] if boolean spec matches boolean argument', function(){
                 var errorArray = argumentSpec.validate('boolean', true, true);
-                assert.equal(0, errorArray.length);
+                expect(errorArray.length).to.equal(0);
             })
 
             it('should return [error] if boolean spec fails to match', function(){
                 var errorArray = argumentSpec.validate('boolean', true, 0);
-                assert.equal(1, errorArray.length);
-                assert.equal('boolean is not a boolean', errorArray[0]);
+                expect(errorArray.length).to.equal(1);
+                expect(errorArray[0]).to.equal('boolean is not a boolean');
             })
         })
 
         describe('number tests', function(){
             it('should return [] if number spec matches number argument', function(){
                 var errorArray = argumentSpec.validate('number', 0, 0);
-                assert.equal(0, errorArray.length);
+                expect(errorArray.length).to.equal(0);
             })
 
             it('should return [error] if number spec fails to match', function(){
                 var errorArray = argumentSpec.validate('number', 0, true);
-                assert.equal(1, errorArray.length);
-                assert.equal('number is not a number', errorArray[0]);
+                expect(errorArray.length).to.equal(1);
+                expect(errorArray[0]).to.equal('number is not a number');
             })
         })
     })
@@ -41,24 +41,32 @@
     describe('string tests', function(){
         it('should return [] if string spec matches string argument', function(){
             var errorArray = argumentSpec.validate('string', '', 'foo');
-            assert.equal(0, errorArray.length);
+            expect(errorArray.length).to.equal(0);
         })
 
         it('should return [error] if string spec fails to match', function(){
             var errorArray = argumentSpec.validate('string', '', true);
-            assert.equal(1, errorArray.length);
-            assert.equal('string is not a string', errorArray[0]);
+            expect(errorArray.length).to.equal(1);
+            expect(errorArray[0]).to.equal('string is not a string');
         })
 
         it('should return [] if regex spec matches string argument', function(){
             var errorArray = argumentSpec.validate('string', 'foo\\d', 'foo2');
-            assert.equal(0, errorArray.length);
+            expect(errorArray.length).to.equal(0);
         })
 
         it('should return [error] if regex spec fails to match', function(){
             var errorArray = argumentSpec.validate('string', 'foo\\d', 'fooa');
-            assert.equal(1, errorArray.length);
-            assert.equal('string does not match regex', errorArray[0]);
+            expect(errorArray.length).to.equal(1);
+            expect(errorArray[0]).to.equal('string does not match regex');
+        })
+    })
+
+    describe('other scalars', function () {
+        it('should return [error] if spec is null', function(){
+            var errorArray = argumentSpec.validate('isnull', null, 1);
+            expect(errorArray.length).to.equal(1);
+            expect(errorArray[0]).to.equal('isnull spec should not be null');
         })
     })
 
